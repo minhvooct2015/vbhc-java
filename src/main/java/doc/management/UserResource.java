5ace28835124d52cf9c6a4a4565d92ab1f882949
Minh <minhvoct2015@gmail.com>
2025-09-06 19:44:59 +0700

    init

---
+++ src/main/java/doc/management/UserResource.java
@@ -0,0 +1,16 @@
+package doc.management;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/login")
+public class UserResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello from Quarkus REST";
+    }
+}
